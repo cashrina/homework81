6ac94bd1100c63be574b7090cc3e7098db30a9ba
cashrina <khripunova.irina.24@gmail.com>
2024-12-26 01:51:32 +0600

    удален лишний код

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDom from 'react-dom/client';
+import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDom.createRoot(document.getElementById('root')!).render(
+    <App />);
